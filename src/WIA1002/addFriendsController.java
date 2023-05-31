9daca66915e09077a6bc7e603b7e061e4241cec6
GohYH23 <121934266+GohYH23@users.noreply.github.com>
2023-05-31 13:23:18 +0800

    Update addFriendsController.java

--- src/WIA1002/addFriendsController.java
+++ src/WIA1002/addFriendsController.java
@@ -2,12 +2,24 @@ package WIA1002;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class addFriendsController {
     @FXML
     private AnchorPane contentPane;
+
+    private regularUser user;
+
+    public void setUser(regularUser user) {
+        this.user = user;
+    }
 
     @FXML
     private void suggestButtonOnAction() {
@@ -19,14 +31,24 @@ public class addFriendsController {
     private void showRequestButtonOnAction() {
         try {
             // Load the friend request FXML file
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("friendrequest.fxml"));
-            Pane friendRequestPane = loader.load();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("friendRequest.fxml"));
+//            Pane friendRequestPane = loader.load();
+            Parent friendRequest = loader.load();
 
-            // Set the friend request pane as the content of the contentPane
-            contentPane.getChildren().setAll(friendRequestPane);
-        } catch (Exception e) {
+            friendRequestController friendRequestController = loader.getController();
+            friendRequestController.setUser(user);
+
+            Stage friendRequestStage = new Stage();
+            friendRequestStage.initStyle(StageStyle.UNDECORATED);
+            friendRequestStage.setScene(new Scene(friendRequest));
+
+            // Show the friend request screen
+            friendRequestStage.showAndWait();
+
+//            // Set the friend request pane as the content of the contentPane
+//            contentPane.getChildren().setAll(friendRequestPane);
+        } catch (IOException e) {
             e.printStackTrace();
-            e.getCause();
         }
     }
 }
