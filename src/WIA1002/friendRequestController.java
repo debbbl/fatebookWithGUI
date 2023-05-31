e56b2bbccbf60f0459fb3243eb7ef731b14bc720
GohYH23 <121934266+GohYH23@users.noreply.github.com>
2023-05-31 13:18:30 +0800

    Update friendRequestController.java

--- src/WIA1002/friendRequestController.java
+++ src/WIA1002/friendRequestController.java
@@ -10,12 +10,17 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
 import java.io.ByteArrayInputStream;
 import java.net.URL;
 import java.sql.*;
 import java.util.ResourceBundle;
 
 public class friendRequestController implements Initializable {
+    @FXML
+    private Button cancelButton;
+
     @FXML
     private ListView<String> friendRequestListView;
     @FXML
@@ -32,17 +37,25 @@ public class friendRequestController implements Initializable {
     private String username;
     private ObservableList<String> friendRequests;
 
+    private regularUser user;
+
+    public void setUser(regularUser user) {
+        this.user = user;
+        setUserDetails(user.getUserId(),user.getUsername());
+
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         database = new tempDatabase();
         friendRequests = FXCollections.observableArrayList();
 
         friendRequestListView.setItems(friendRequests);
-        friendRequestListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-                showFriendRequestDetails(newValue);
-            }
-        });
+//        friendRequestListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+//            if (newValue != null) {
+//                showFriendRequestDetails(newValue);
+//            }
+//        });
     }
 
     public void setUserDetails(int userId, String username) {
@@ -54,19 +67,24 @@ public class friendRequestController implements Initializable {
     private void loadFriendRequests() {
         Connection connection = database.getConnection();
         try {
-            String query = "SELECT * FROM friendrequest WHERE user_id = ?";
+            String query = "SELECT * FROM friendrequest WHERE username = ?";
             PreparedStatement statement = connection.prepareStatement(query);
-            statement.setInt(1, userId);
+            statement.setString(1, username);
             ResultSet resultSet = statement.executeQuery();
 
             while (resultSet.next()) {
-                int friendRequestId = resultSet.getInt("request_id");
-                int friendId = resultSet.getInt("friend_id");
-                String friendUsername = resultSet.getString("friend_username");
-                boolean accepted = resultSet.getBoolean("accepted_request");
+                int friendRequestId = resultSet.getInt("user_id");
+                // int friendId = resultSet.getInt("friend_id");
+                String friendUsername = resultSet.getString("requestReceived");
+                String [] friend = friendUsername.split(";");
+                String accepted = resultSet.getString("accepted_request");
 
-                String friendRequest = "ID: " + friendRequestId + ", Username: " + friendUsername + ", Accepted: " + accepted;
-                friendRequests.add(friendRequest);
+                for(String friends : friend)
+                {
+                    String friendRequest = "ID: " + friendRequestId + ", Username: " + friends + ", Accepted: " + accepted; //the id can be throw as it display the current user id
+                    friendRequests.add(friendRequest);
+                }
+
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -89,9 +107,10 @@ public class friendRequestController implements Initializable {
     @FXML
     private void acceptButtonClicked() {
         String selectedFriendRequest = friendRequestListView.getSelectionModel().getSelectedItem();
+
         if (selectedFriendRequest != null) {
             int friendRequestId = Integer.parseInt(selectedFriendRequest.substring(selectedFriendRequest.indexOf(":") + 2, selectedFriendRequest.indexOf(",")));
-            updateFriendRequest(friendRequestId, true);
+            updateFriendRequest(friendRequestId);
             friendRequests.remove(selectedFriendRequest);
 
             showAlert(Alert.AlertType.INFORMATION, "Friend Request Accepted", "You have accepted the friend request.");
@@ -103,7 +122,7 @@ public class friendRequestController implements Initializable {
         String selectedFriendRequest = friendRequestListView.getSelectionModel().getSelectedItem();
         if (selectedFriendRequest != null) {
             int friendRequestId = Integer.parseInt(selectedFriendRequest.substring(selectedFriendRequest.indexOf(":") + 2, selectedFriendRequest.indexOf(",")));
-            updateFriendRequest(friendRequestId, false);
+            updateFriendRequestReject(friendRequestId);
             friendRequests.remove(selectedFriendRequest);
 
             showAlert(Alert.AlertType.INFORMATION, "Friend Request Rejected", "You have rejected the friend request.");
@@ -120,13 +139,16 @@ public class friendRequestController implements Initializable {
         }
     }
 
-    private void updateFriendRequest(int friendRequestId, boolean accept) {
+    private void updateFriendRequest(int friendRequestId) {
         Connection connection = database.getConnection();
+        String selectedFriendRequest = friendRequestListView.getSelectionModel().getSelectedItem();
+        String usernameToRemove = getUsernameFromFriendRequest(selectedFriendRequest);
         try {
-            String updateQuery = "UPDATE friendrequest SET accepted_request = ? WHERE request_id = ?";
+            String updateQuery = "UPDATE friendrequest SET accepted_request = CONCAT(IFNULL(accepted_request, ''), ?), requestReceived = REPLACE(requestReceived, ?, '') WHERE user_id = ?";
             PreparedStatement statement = connection.prepareStatement(updateQuery);
-            statement.setBoolean(1, accept);
-            statement.setInt(2, friendRequestId);
+            statement.setString(1, usernameToRemove + ";");
+            statement.setString(2, usernameToRemove+";");
+            statement.setInt(3, friendRequestId);
             statement.executeUpdate();
             System.out.println("Friend request updated successfully!");
         } catch (SQLException e) {
@@ -140,10 +162,46 @@ public class friendRequestController implements Initializable {
         }
     }
 
+    private void updateFriendRequestReject(int friendRequestId) {
+        Connection connection = database.getConnection();
+        String selectedFriendRequest = friendRequestListView.getSelectionModel().getSelectedItem();
+        String usernameToRemove = getUsernameFromFriendRequest(selectedFriendRequest);
+        try {
+            String updateQuery = "UPDATE friendrequest SET rejected_request = CONCAT(IFNULL(rejected_request, ''), ?), requestReceived = REPLACE(requestReceived, ?, '') WHERE user_id = ?";
+            PreparedStatement statement = connection.prepareStatement(updateQuery);
+            statement.setString(1, usernameToRemove + ";");
+            statement.setString(2, usernameToRemove+";");
+            statement.setInt(3, friendRequestId);
+            statement.executeUpdate();
+            System.out.println("Friend request updated successfully!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String getUsernameFromFriendRequest(String friendRequest) {
+        String[] parts = friendRequest.split(",");
+        String username = parts[1].substring(parts[1].indexOf(":") + 2);
+        return username;
+    }
+
     private void showAlert(Alert.AlertType alertType, String title, String message) {
         Alert alert = new Alert(alertType);
         alert.setTitle(title);
         alert.setContentText(message);
         alert.showAndWait();
+    }
+
+    @FXML
+    private void cancelButtonOnAction() {
+        // Close the Edit Account screen
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
     }
 }
