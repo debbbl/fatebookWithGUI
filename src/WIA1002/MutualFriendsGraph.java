37cc7a739c99822bbcfab6f88be74d92a58f8654
GohYH23 <121934266+GohYH23@users.noreply.github.com>
2023-06-04 20:14:48 +0800

    Update MutualFriendsGraph.java

--- src/WIA1002/MutualFriendsGraph.java
+++ src/WIA1002/MutualFriendsGraph.java
@@ -3,24 +3,19 @@ package WIA1002;
 import java.util.*;
 
 public class MutualFriendsGraph {
-    private Map<String, Set<String>> graph;
+    private Graph<String,String> graph;
 
     public MutualFriendsGraph() {
-        graph = new HashMap<>();
+
+        graph = new Graph<>();
     }
 
-    public void addFriendship(String user1, String user2) {
-        graph.computeIfAbsent(user1, k -> new HashSet<>()).add(user2);
-        graph.computeIfAbsent(user2, k -> new HashSet<>()).add(user1);
+    public void setGraph(Graph<String, String> graph) {
+        this.graph = graph;
     }
 
-    public List<String> getMutualFriends(String user1, String user2) {
-        Set<String> friends1 = graph.getOrDefault(user1, new HashSet<>());
-        Set<String> friends2 = graph.getOrDefault(user2, new HashSet<>());
-
-        Set<String> mutualFriends = new HashSet<>(friends1);
-        mutualFriends.retainAll(friends2);
-
-        return new ArrayList<>(mutualFriends);
+    public ArrayList<String> findMutualFriends(String user_username, String user1_username)
+    {
+        return graph.findMutualFriends(user_username,user1_username);
     }
 }
